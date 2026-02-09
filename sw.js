8ba82d9d439c49f31689c9644a6c24e4d2b0e529
Dennis Araya Bustamante <dennis.araya@gmail.com>
2026-02-09 14:45:29 -0300

    Add Service Worker for caching assets

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'miso-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Instalar el Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Responder con la caché si no hay internet
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
