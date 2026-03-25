dc108f17abfe3276c59c5b6a5b312253ca060da7
Dennis Araya Bustamante <dennis.araya@gmail.com>
2026-03-25 17:58:36 +0000

    Botoneras grandes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const HOST = process.env.HOST || '0.0.0.0';
 const PACKAGE_FILE = path.join(__dirname, 'package.json');
 const DATA_DIR = path.join(__dirname, 'data');
